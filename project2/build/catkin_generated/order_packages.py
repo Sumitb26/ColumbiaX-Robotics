4d3e88dc5575e077d9a543daf1bac904bdb62380
Sumitb26 <bhirud.sumit@gmail.com>
2018-07-18 21:33:12 +0530

    added project2

---
+++ project2/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/sumit/project2/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/sumit/project3/devel;/home/sumit/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/sumit/project3/devel;/home/sumit/catkin_ws/devel;/opt/ros/kinetic" != "" else []
