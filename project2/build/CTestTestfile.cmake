82e79c9a0eeadfd4935932d0358d9740719ec826
Sumitb26 <bhirud.sumit@gmail.com>
2018-07-23 21:37:10 +0530

    clean up

--- project2/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sumit/project2/src
-# Build directory: /home/sumit/project2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(marker_publisher)
-subdirs(project2_solution)
--- project2/build/marker_publisher/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sumit/project2/src/marker_publisher
-# Build directory: /home/sumit/project2/build/marker_publisher
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
