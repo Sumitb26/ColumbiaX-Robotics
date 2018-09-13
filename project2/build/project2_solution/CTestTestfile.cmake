58ae9af54969509f5dd3b35e967792046f68cdff
Sumitb26 <bhirud.sumit@gmail.com>
2018-09-13 22:44:10 +0530

    project2

--- project2/build/project2_solution/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sumit/project2/src/project2_solution
-# Build directory: /home/sumit/project2/build/project2_solution
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
